9a8c9a247687b2efd972aa2fc1454f0ef15d3127
Jeffrey Sun <jeffrey.zksun@gmail.com>
2013-09-24 16:17:14 +0800

    Add the code sample

---
+++ src/restcsharp/restcsharp/RestAPI.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace restcsharp
+{
+    public class RestAPI
+    {
+        public RestAPI()
+        {
+            // Set the default value
+            APIEndPoint = "http://127.0.0.1:3000";
+            BaseURL = "/api/1.0";
+            AuthoId = "";
+        }
+        public String APIEndPoint { get; set; } // http://127.0.0.1:3000
+        public String BaseURL {get; set;} // /api/1.0
+        public String AuthoId { get; set; } // 21XW2DS254
+
+        public bool SendHttpRequest(string uri, string method, JObject bodyObject, out JObject responseObject)
+        {
+            responseObject = null;
+            JContainer response;
+            bool ret = _SendHttpRequest(uri, method, bodyObject, out response);
+            if (ret && response != null)
+            {
+                responseObject = response as JObject;
+            }
+
+            return ret;
+        }
+
+        public bool SendHttpRequest(string uri, string method, JObject bodyObject, out JArray responseObject)
+        {
+            responseObject = null;
+            JContainer response;
+            bool ret = _SendHttpRequest(uri, method, bodyObject, out response);
+            if (ret && response != null)
+            {
+                responseObject = response as JArray;
+            }
+
+            return ret;
+        }
+
+        /**
+         * @param uri
+         * @param method Case-insensitive
+         * @param bodyObject 
+         * @param responseObject output, the value would be JObject or JArray        
+         */
+        private bool _SendHttpRequest(string uri, string method, JObject bodyObject, out JContainer responseObject)
+        {
+            responseObject = null;
+            var url = APIEndPoint + BaseURL + @"/" + uri;
+
+            // Set remote url and http method.
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = method;
+
+            // Set the content type header and http body.
+            if(bodyObject != null){
+                var body = JsonConvert.SerializeObject(bodyObject);
+                if (body.Length != 0)
+                {
+                    request.ContentType = "application/json";
+                    using (Stream requestStream = request.GetRequestStream())
+                    using (StreamWriter writer = new StreamWriter(requestStream))
+                    {
+                        writer.Write(body);
+                    }
+                }
+            }
+
+            // Set the authorization id.
+            string sAuth = "auth_id=" + AuthoId;
+            request.Headers.Add("Authorization", sAuth);
+
+            string responseData = "";
+            try
+            {
+                // Send request
+                System.Net.WebResponse response = (HttpWebResponse)request.GetResponse();
+                responseData = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            }
+            catch (WebException e)
+            {
+                HttpWebResponse response = (HttpWebResponse)e.Response;
+                if (response != null)
+                {
+                    responseData = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+                    response.Close();   // Releases the resources of the response.
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            if (!string.IsNullOrEmpty(responseData))
+            {
+                var res = JsonConvert.DeserializeObject(responseData);
+                responseObject = res as JContainer;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/restcsharp/restcsharp/Sample.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace restcsharp
+{
+    class Sample
+    {
+        public static int  Main(){
+
+            RestAPI restAPI = new RestAPI() { APIEndPoint = "http://127.0.0.1:3000" };
+            var res_id = "1227f8e0-2367-11e3-95a6-d5147a878154";
+            JObject order = null;
+
+            // Create restaurant
+            Console.WriteLine(">> Create restaurant");
+            {
+                // {"name":"湘菜馆","address":" No. 34 Tianlin Rd.","description":"Cool","telphones":["15933822749","021-66251973"],"logo":""}
+                JObject requestData = new JObject();
+                requestData.Add("name", "湘菜馆");
+                requestData.Add("address", "No. 34 Tianlin Rd.");
+                requestData.Add("description", "正宗湘菜");
+                var telphones = new JArray();
+                telphones.Add("15933822749");
+                telphones.Add("021-66251973");
+                requestData.Add("telphones", telphones);
+                requestData.Add("logo", "");
+
+                var uri = "restaurants";
+                var method = "post";
+
+                JObject response = null;
+                bool ret = restAPI.SendHttpRequest(uri, method, requestData, out response);
+                if (!ret)
+                {
+                    Console.Write("fail");
+                    return 1;
+                }
+                else{
+                    var strBody = JsonConvert.SerializeObject(response);
+                    Console.WriteLine(strBody);
+                    //res_id = response["id"].ToString();
+                }
+            }
+            // Get order
+            Console.WriteLine(">> Get order");
+            {
+                
+                var uri = "restaurants/" + res_id + "/orders";
+                var method = "GET";
+
+                JArray response = null;
+                bool ret = restAPI.SendHttpRequest(uri, method, null, out response);
+                if (!ret)
+                {
+                    Console.Write("fail");
+                    return 1;
+                }
+                else
+                {
+                    var strBody = JsonConvert.SerializeObject(response);
+                    Console.WriteLine(strBody);
+
+                    order = response[0] as JObject; 
+                }
+            }
+
+            // Update order
+            Console.WriteLine(">> Update order");
+            {
+                // {"items":[{"item":{"type":"course","id":"68ca6820-24db-11e3-87c1-f12296dee4f7"},"quantity":2}]}
+
+                // Get int
+                // http://stackoverflow.com/questions/9589218/get-value-from-jtoken-that-may-not-exist-best-practices                
+                var status = order["status"].ToString();
+
+                // The link to modify the jobject
+                // http://stackoverflow.com/questions/3206345/how-can-i-best-utilize-json-net-to-modify-parts-of-an-existing-json-object
+                JValue newStatus = new JValue("printed");
+                order["status"].Replace(newStatus);
+
+                var uri = "orders/" + order["id"];
+                var method = "PUT";
+
+                JObject response = null;
+                bool ret = restAPI.SendHttpRequest(uri, method, order, out response);
+                if (!ret)
+                {
+                    Console.Write("fail");
+                    return 1;
+                }
+                else
+                {
+                    var strBody = JsonConvert.SerializeObject(response);
+                    Console.WriteLine(strBody);
+                }
+            } 
+
+            return 0;
+        }
+    }
+}
